a9f3ade7e57b7172304499752c8369ea9e86569f
Ben DiFrancesco <ben@scopelift.co>
2017-06-15 16:42:27 -0400

    Create a tab for displaying patients and connect it to a split view controller

---
+++ BackPortal/ViewControllers/PatientSplitViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class PatientSplitViewController: UISplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("[PORTAL] Hello Patient Split")
+    }
+}
