e63a57eb39695ad67dacf174910d1d1442b6a1fa
Ben DiFrancesco <ben@scopelift.co>
2017-06-20 14:12:42 -0400

    Display the number and status of each intervention event
    Using a horizontal stack view, create and add styled circles
    that are filled in if the event is completed. This implementation
    has a known limitation, that it falls down if the number of
    events results in views which are longer than the stack view.
    Programatically applied constraints might work better, but we
    can probably live with this for the demo.

--- BackPortal/Views/InterventionCell.swift
+++ BackPortal/Views/InterventionCell.swift
@@ -7,6 +7,7 @@ class InterventionCell: UICollectionViewCell {
     
     @IBOutlet fileprivate var nameLabel: UILabel?
     @IBOutlet fileprivate var subLabel: UILabel?
+    @IBOutlet fileprivate var eventsStackView: UIStackView?
     
     // MARK: Public
     
@@ -16,10 +17,52 @@ class InterventionCell: UICollectionViewCell {
         }
         
         onMain {
+            self.resetStackView()
+            
             self.layer.cornerRadius = 6.0
             
             self.nameLabel?.text = activity.title
             self.subLabel?.text = activity.text
+            
+            events.forEach { event in
+                let view = UIView()
+                
+                let size = (self.eventsStackView?.bounds.height ?? 36.0)
+                view.heightAnchor.constraint(equalToConstant: size).isActive = true
+                view.widthAnchor.constraint(equalToConstant: size).isActive = true
+                
+                view.layer.borderWidth = 2.0
+                view.layer.borderColor = activity.tintColor?.cgColor
+                view.layer.cornerRadius = size / 2.0
+                
+                if case .completed = event.state {
+                    view.backgroundColor = activity.tintColor
+                }
+                
+                self.eventsStackView?.addArrangedSubview(view)
+                print("[PORTAL] View Height: \(view.bounds.height)")
+            }
+            
+            let spaceView = UIView()
+            let size = (self.eventsStackView?.bounds.height ?? 36.0)
+            spaceView.heightAnchor.constraint(equalToConstant: size).isActive = true
+            spaceView.widthAnchor.constraint(greaterThanOrEqualToConstant: size).isActive = true
+            spaceView.backgroundColor = UIColor.clear
+            
+            self.eventsStackView?.addArrangedSubview(spaceView)
+        }
+    }
+    
+    // MARK: Private
+    
+    func resetStackView() {
+        guard let eventsStackView = eventsStackView else {
+            return
+        }
+        
+        while eventsStackView.arrangedSubviews.count > 0 {
+            eventsStackView.arrangedSubviews[0].removeFromSuperview()
+            //eventsStackView.removeArrangedSubview(eventsStackView.arrangedSubviews[0])
         }
     }
 }
