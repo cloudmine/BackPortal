7b51f8d360aa2b3a05c39cd2c63749fd1b0a9936
Ben DiFrancesco <ben@scopelift.co>
2017-06-21 16:03:00 -0400

    Display the Assessment activity name and subtext in the activies list

--- BackPortal/Views/AssessmentCell.swift
+++ BackPortal/Views/AssessmentCell.swift
@@ -5,10 +5,19 @@ typealias AssessmentEventTapCallback = (OCKCarePlanEvent) -> Void
 
 class AssessmentCell: UICollectionViewCell {
     
+    @IBOutlet fileprivate var nameLabel: UILabel?
+    @IBOutlet fileprivate var subLabel: UILabel?
+    
     func configure(with eventList: [OCKCarePlanEvent], tapBack: AssessmentEventTapCallback?) {
+        guard let activity = eventList.first?.activity else {
+            return
+        }
         
         onMain {
             self.layer.cornerRadius = ActivityCellCornerRadius
+            
+            self.nameLabel?.text = activity.title
+            self.subLabel?.text = activity.text
         }
     }
 }
