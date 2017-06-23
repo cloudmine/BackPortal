4c1976aaa740f335ea3ed1b646b1682a4f6af6df
Ben DiFrancesco <ben@scopelift.co>
2017-06-23 13:45:25 -0400

    Show a Date Picker for the Patient
    * Add a bar button to the patient detail view
    * Show a date picker in a popover if the user selects the button
    * Capture the date selected when the popover is dismissed

--- BackPortal/ViewControllers/PatientDetailViewController.swift
+++ BackPortal/ViewControllers/PatientDetailViewController.swift
@@ -1,6 +1,14 @@
 import UIKit
 
 class PatientDetailViewController: UIViewController {
+    
+    // MARK: Outlets
+    
+    @IBOutlet var dateButton: UIBarButtonItem?
+    
+    // MARK: Private Variables
+    
+    fileprivate var lastSelectedDate: Date = Date()
 
     // MARK: Lifecycle
     
@@ -19,5 +27,42 @@ class PatientDetailViewController: UIViewController {
         } else {
             navigationItem.title = "Patient"
         }
+    }
+}
+
+// MARK: Taret-Action
+
+extension PatientDetailViewController {
+    
+    @IBAction func didPress(dateButton: UIBarButtonItem) {
+        let datePickVC = UIViewController()
+        datePickVC.modalPresentationStyle = .popover
+        datePickVC.preferredContentSize = CGSize(width: 320, height: 225)
+        datePickVC.popoverPresentationController?.barButtonItem = dateButton
+        datePickVC.popoverPresentationController?.delegate = self
+        
+        let datePickerView = UIView(frame: CGRect(x: 0, y: 0, width: 320, height: 225))
+        
+        let datePicker = UIDatePicker(frame: datePickerView.frame)
+        datePicker.datePickerMode = .date
+        datePicker.addTarget(self, action: #selector(selectedDate(from:)), for: .valueChanged)
+        
+        datePickerView.addSubview(datePicker)
+        datePickVC.view = datePickerView
+        
+        present(datePickVC, animated: true, completion: nil)
+    }
+    
+    func selectedDate(from datePicker: UIDatePicker) {
+        lastSelectedDate = datePicker.date
+    }
+}
+
+// MARK: UIPopoverPresentationControllerDelegate
+
+extension PatientDetailViewController: UIPopoverPresentationControllerDelegate {
+    
+    func popoverPresentationControllerDidDismissPopover(_ popoverPresentationController: UIPopoverPresentationController) {
+        print("[PORTAL] Date Picked: \(lastSelectedDate)")
     }
 }
