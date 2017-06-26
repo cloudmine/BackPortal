de787380ba59c85c0eb7c272b60702acc276eebc
Ben DiFrancesco <ben@scopelift.co>
2017-06-26 17:11:14 -0400

    Create Full OCKCarePlanActivity from ResearchKit Task
    For now, applies only for creating an exercise intervention
    activity. Extracts all relevant data from the ORKTaskResult
    and assembles the appropriate OCKCarePlanActivity.

--- BackPortal/Models/NewActivityTasks.swift
+++ BackPortal/Models/NewActivityTasks.swift
@@ -17,23 +17,25 @@ struct NewActitiviesTasks {
     static func carePlanActivity(from result:ORKTaskResult) -> OCKCarePlanActivity? {
         guard
             "PortalAddInterventionActivity" == result.identifier,
-            let title = title(from: result)
+            let title = title(from: result),
+            let repetitions = repetitions(from: result),
+            let schedule = schedule(from: result, repetitions: repetitions)
         else {
             return nil
         }
         
-        let todayComponents = NSDateComponents(date: Date(), calendar: Calendar.current) as DateComponents
-        let schedule = OCKCareSchedule.weeklySchedule(withStartDate: todayComponents,
-                                                      occurrencesOnEachDay: [1, 1, 1, 1, 1, 1, 1])
+
         let identifier = "BCM\(title)InterventionActivity"
+        let instructions = self.instructions(from: result)
+        let description = self.description(from: result)
         
         return OCKCarePlanActivity(identifier: identifier,
                                    groupIdentifier: nil,
                                    type: .intervention,
                                    title: title,
-                                   text: nil,
+                                   text: description,
                                    tintColor: nil,
-                                   instructions: nil,
+                                   instructions: instructions,
                                    imageURL: nil,
                                    schedule: schedule,
                                    resultResettable: true,
@@ -46,10 +48,12 @@ struct NewActitiviesTasks {
 
 fileprivate extension NewActitiviesTasks {
     
+    static let ExercieNameQuestionIdentifier = "PortalAddExerciseName"
+    
     static var ExerciseNameQuestion: ORKQuestionStep {
         let answer = ORKTextAnswerFormat(maximumLength: 50)
         
-        let question = ORKQuestionStep(identifier: "PortalAddExerciseName",
+        let question = ORKQuestionStep(identifier: ExercieNameQuestionIdentifier,
                                            title: NSLocalizedString("Exercise Name", comment: ""),
                                            text: NSLocalizedString("Enter the title for this exercie activity", comment: ""),
                                            answer: answer)
@@ -58,13 +62,15 @@ fileprivate extension NewActitiviesTasks {
         return question
     }
     
+    static let ExerciseTimeQuestionIdentifier = "PortalAddExerciseTime"
+    
     static var ExerciseTimeQuestion: ORKQuestionStep {
         let answer = ORKNumericAnswerFormat(style: .integer,
                                             unit: NSLocalizedString("minutes", comment: ""),
                                             minimum: NSNumber(value: 1),
                                             maximum: NSNumber(value: 120))
         
-        let question = ORKQuestionStep(identifier: "PortalAddExerciseTime",
+        let question = ORKQuestionStep(identifier: ExerciseTimeQuestionIdentifier,
                                        title: NSLocalizedString("Duration", comment: ""),
                                        text: NSLocalizedString("Enter the prescribed exercise duration in minutes", comment: ""),
                                        answer: answer)
@@ -73,20 +79,21 @@ fileprivate extension NewActitiviesTasks {
         return question
     }
     
+    static let ExerciseDaysQuesitonIdentifier = "PortalAddExerciseDays"
+    
     static var ExerciseDaysQuestion: ORKQuestionStep {
-        let choices = [ORKTextChoice(text: NSLocalizedString("Monday", comment: ""), value: "M" as NSString),
-                       ORKTextChoice(text: NSLocalizedString("Tuesday", comment: ""), value: "T" as NSString),
-                       ORKTextChoice(text: NSLocalizedString("Wednesday", comment: ""), value: "W" as NSString),
-                       ORKTextChoice(text: NSLocalizedString("Thursday", comment: ""), value: "R" as NSString),
-                       ORKTextChoice(text: NSLocalizedString("Friday", comment: ""), value: "F" as NSString),
-                       ORKTextChoice(text: NSLocalizedString("Saturday", comment: ""), value: "S" as NSString),
-                       ORKTextChoice(text: NSLocalizedString("Sunday", comment: ""), value: "N" as NSString),]
-                       
-                       
+        let choices = [ ORKTextChoice(text: NSLocalizedString("Sunday", comment: ""), value: "Su" as NSString),
+                        ORKTextChoice(text: NSLocalizedString("Monday", comment: ""), value: "Mo" as NSString),
+                        ORKTextChoice(text: NSLocalizedString("Tuesday", comment: ""), value: "Tu" as NSString),
+                        ORKTextChoice(text: NSLocalizedString("Wednesday", comment: ""), value: "We" as NSString),
+                        ORKTextChoice(text: NSLocalizedString("Thursday", comment: ""), value: "Th" as NSString),
+                        ORKTextChoice(text: NSLocalizedString("Friday", comment: ""), value: "Fr" as NSString),
+                        ORKTextChoice(text: NSLocalizedString("Saturday", comment: ""), value: "Sa" as NSString), ]
+        
         let answer = ORKTextChoiceAnswerFormat(style: .multipleChoice,
                                                textChoices: choices)
         
-        let question = ORKQuestionStep(identifier: "PortalAddExerciseDays",
+        let question = ORKQuestionStep(identifier: ExerciseDaysQuesitonIdentifier,
                                        title: NSLocalizedString("Days of Week", comment: ""),
                                        text: NSLocalizedString("Select the days of the week the patient should complete this exercise", comment: ""),
                                        answer: answer)
@@ -95,13 +102,15 @@ fileprivate extension NewActitiviesTasks {
         return question
     }
     
+    static let ExerciseRepetitionsQuestionIdentifier = "PortalAddExerciseRepetitions"
+    
     static var ExerciseRepetitionsQuestion: ORKQuestionStep {
         let answer = ORKNumericAnswerFormat(style: .integer,
                                             unit: nil,
                                             minimum: NSNumber(value: 1),
                                             maximum: NSNumber(value: 5))
         
-        let question = ORKQuestionStep(identifier: "PortalAddExerciseRepetitions",
+        let question = ORKQuestionStep(identifier: ExerciseRepetitionsQuestionIdentifier,
                                        title: NSLocalizedString("Daily Repetitions", comment: ""),
                                        text: NSLocalizedString("How many times, per day, should the patient complete this exercise", comment: ""),
                                        answer: answer)
@@ -110,10 +119,12 @@ fileprivate extension NewActitiviesTasks {
         return question
     }
     
+    static let ExerciseInstructionsQuestionIdentifier = "PortalAddExerciseInstructions"
+    
     static var ExerciseInstructionsQuestion: ORKQuestionStep {
         let answer = ORKTextAnswerFormat(maximumLength: 300)
         
-        let question = ORKQuestionStep(identifier: "PortalAddExerciseInstructions",
+        let question = ORKQuestionStep(identifier: ExerciseInstructionsQuestionIdentifier,
                                        title: NSLocalizedString("Instructions", comment: ""),
                                        text: NSLocalizedString("Enter the instructions for the patient to follow for this exercise", comment: ""),
                                        answer: answer)
@@ -129,7 +140,58 @@ fileprivate extension NewActitiviesTasks {
     
     static func title(from taskResult: ORKTaskResult) -> String? {
         return
-            extract(from: taskResult, identifier: "PortalAddExerciseName", extractor: { (textResult: ORKTextQuestionResult)in
+            extract(from: taskResult, identifier: ExercieNameQuestionIdentifier, extractor: { (textResult: ORKTextQuestionResult) in
+                return textResult.textAnswer
+            })
+    }
+    
+    static func repetitions(from taskResult: ORKTaskResult) -> Int? {
+        return
+            extract(from: taskResult, identifier: ExerciseRepetitionsQuestionIdentifier, extractor: { (numResult: ORKNumericQuestionResult) in
+                return numResult.numericAnswer?.intValue
+            })
+    }
+    
+    static func description(from taskResult: ORKTaskResult) -> String? {
+        return
+            extract(from: taskResult, identifier: ExerciseTimeQuestionIdentifier, extractor: { (numResult: ORKNumericQuestionResult) in
+                guard
+                    let value = numResult.numericAnswer as? Int,
+                    let units = numResult.unit
+                else {
+                        return nil
+                }
+                
+                return "\(value) \(units)"
+            })
+    }
+    
+    static func schedule(from taskResult: ORKTaskResult, repetitions: Int) -> OCKCareSchedule? {
+        guard
+            let dayList =
+            extract(from: taskResult,
+                    identifier: ExerciseDaysQuesitonIdentifier,
+                    extractor: { (choiceResult: ORKChoiceQuestionResult) -> ([String]?) in
+                        return choiceResult.choiceAnswers as? [String]
+                    })
+        else {
+            return nil
+        }
+        
+        let repSchedule = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"]
+                            .flatMap({ dayList.contains($0) ? 1 : 0 })
+                            .map({ $0 * repetitions })
+                            .map({ NSNumber(value: $0) })
+        
+        let todayComponents = NSDateComponents(date: Date(), calendar: Calendar.current) as DateComponents
+        
+        return OCKCareSchedule.weeklySchedule(withStartDate: todayComponents,
+                                              occurrencesOnEachDay: repSchedule)
+    }
+    
+    static func instructions(from taskResult: ORKTaskResult) -> String? {
+        return
+            extract(from: taskResult, identifier: ExerciseInstructionsQuestionIdentifier, extractor: { (textResult: ORKTextQuestionResult) in
                 return textResult.textAnswer
             })
     }
